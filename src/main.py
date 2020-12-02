6571717ab917070eeea20f3e885e16df40e54485
Alex Shokolarov <shokolarow123@gmail.com>
2020-12-02 15:08:52 +0100

    Functionality to convert state vector to keplerian elements added

--- src/main.py
+++ src/main.py
@@ -6,17 +6,17 @@ central_body = EARTH
 satellites = [ISS]
 
 # Keplerian orbital elements of sats
-koe = [[EARTH['radius'] + 3590e3, 0.032, 87.87, 53.39, 227.9, 0.0]]
+koe = [[EARTH['radius'] + 35000e3, 0.032, 87.87, 53.39, 227.9, 0.0]]
 
 properties = {
-       'J2' : False,
+       'J2' : True,
        'Drag' : False,
        'min_alt' : 200e3,
        'exit_upon_stopcondition' : False
 }
 
-T_TOTAL = 24 * 60 * 60
-dt = 5.                # Time-step [s]
+T_TOTAL = 30 * 24 * 60 * 60
+dt = 30.                # Time-step [s]
 N = int(T_TOTAL / dt)   # Number of nodes
 
 sim = TwoBodyPropagator.from_koe(koe, central_body, satellites, properties, N, dt)
